8ce1d174f5205d70a21755766d0b8f81c076af48
lucero garcia <ml17ft@gmail.com>
2023-02-21 21:28:53 -0600

    Change parameter

--- vars/functions.groovy
+++ vars/functions.groovy
@@ -1,9 +1,9 @@
 13 lines (11 sloc)  397 Bytes
 
 def build(Map params){
-    sh "echo 'this is building ${params.filePath} ${params.DockerImage}"
+    sh "echo 'this is building ${params.filePath} ${params.dockerImage}"
 }
 
 def push(Map params){
-    sh "echo  'pushing ${params.DockerImage}'"
+    sh "echo  'pushing ${params.dockerImage}'"
 }
