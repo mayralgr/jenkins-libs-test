71b08dd64d5ea1f973a93a2d89c1110153b252ba
lucero garcia <ml17ft@gmail.com>
2023-02-21 21:21:59 -0600

    Refactor

--- vars/functions.groovy
+++ vars/functions.groovy
@@ -1,7 +1,7 @@
 13 lines (11 sloc)  397 Bytes
 
 def build(Map params){
-    sh "echo 'this is building ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}'"
+    sh "echo 'this is building ${params.filePath} ${params.DockerImage}"
 }
 
 def push(Map params){
